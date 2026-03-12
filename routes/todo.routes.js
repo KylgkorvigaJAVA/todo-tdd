c67aa06cb843dd5eba0b2e19d43628b5cbc62cac
Kaspar <kylgkorvigajava@gmail.com>
2026-03-12 12:33:43 +0200

    Create TodoController.getTodos unit test and integration tests

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const todoController = require('../controllers/todo.controller');
+const { route } = require('../app');
 
 const router = express.Router();
 
 router.post('/', todoController.createTodo)
+router.get('/', todoController.getTodos)
 
 module.exports = router;
